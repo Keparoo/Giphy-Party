2a9974b8f922b9896b8c3f2bb69dee03e7602041
Kep <kep@MissionControl.localdomain>
2021-09-23 17:05:46 -0400

    Refactor create addGif

--- app.js
+++ app.js
@@ -8,10 +8,8 @@ const searchButton = document.querySelector('#searchButton')
 const removeButton = document.querySelector('#removeButton')
 const gifs = document.querySelector('#gifs')
 
-async function getGiphy(payload) {
-    let res = await axios.get(baseUrl, {params: payload})
+async function addGif(res) {
     const numGifs = res.data.data.length
-    console.log(numGifs)
     if (numGifs) {
         const randGif = Math.floor(Math.random() * numGifs);
         newImg = document.createElement('IMG')
@@ -21,13 +19,14 @@ async function getGiphy(payload) {
 
 }
 
-searchButton.addEventListener('click', function(e) {
+searchButton.addEventListener('click', async function(e) {
     const searchTerm = searchTermEl.value
-    payload = {q: searchTerm, api_key: apiKey}
-
-    getGiphy(payload)
-
     searchTermEl.value = ""
+
+    payload = {q: searchTerm, api_key: apiKey}
+    const res = await axios.get(baseUrl, {params: payload})
+
+    addGif(res)
 })
 
 removeButton.addEventListener('click', function(e) {
