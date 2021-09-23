c0da1a8284939c88b45439149bb1aac42b30edc1
Kep <kep@MissionControl.localdomain>
2021-09-23 16:50:02 -0400

    Create getGiphy

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 console.log("Let's get this party started!");
+
+const baseUrl = 'http://api.giphy.com/v1/gifs/search'
+const apiKey = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym'
+
+const searchTermEl = document.querySelector('#searchTerm')
+const searchButton = document.querySelector('#searchButton')
+const removeButton = document.querySelector('#removeButton')
+const gifs = document.querySelector('#gifs')
+
+async function getGiphy(payload) {
+    let res = await axios.get(baseUrl, {params: payload})
+    // console.log(res.data.data[0].images.downsized.url)
+    newImg = document.createElement('IMG')
+    newImg.setAttribute('src', res.data.data[0].images.downsized.url)
+    gifs.append(newImg)
+}
+
+searchButton.addEventListener('click', function(e) {
+    const searchTerm = searchTermEl.value
+    payload = {q: searchTerm, api_key: apiKey}
+
+    getGiphy(payload)
+
+    searchTermEl.value = ""
+})
+
+removeButton.addEventListener('click', function(e) {
+    gifs.innerHTML = ''
+})
